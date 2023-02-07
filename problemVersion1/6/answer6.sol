f609e7e2c08f65351262a40a2518a74abf2f82f1
chen-yanlong <alan900801@gmail.com>
2023-02-08 00:10:57 +0800

    add problem 6

---
+++ problemVersion1/6/answer6.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache License
+pragma solidity ^0.8.17; 
+contract answer6 {
+    event Log(string method, uint256 amount);
+
+    fallback(bytes calldata input) external payable returns (bytes memory) {
+      emit Log("fallback", msg.value);
+      return input;
+    }
+
+    receive() external payable {
+      emit Log("recieve", msg.value);
+    }
+
+}
