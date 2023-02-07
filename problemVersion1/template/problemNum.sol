8c8059e81b65bd7103bd27ff8b727b6fed6a2a3a
chen-yanlong <alan900801@gmail.com>
2023-02-07 15:20:00 +0800

    add problem1

---
+++ problemVersion1/template/problemNum.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: Apache License
+
+// TODO: You should declare the compiler version here with the reserved word "pragma"
+contract problem0 { 
+    string public greet = "Hello World!";
+}
