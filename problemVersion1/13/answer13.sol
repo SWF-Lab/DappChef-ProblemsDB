5ea8e507753815b5e0435bcae2176101c6b60d6c
chen-yanlong <alan900801@gmail.com>
2023-02-09 22:04:32 +0800

    add problem 13

---
+++ problemVersion1/13/answer13.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache License
+pragma solidity ^0.8.17;
+
+contract answer13 {
+  struct Food {
+    string name;
+    uint256 price;
+  }
+
+  Food[] public foods;
+
+  function createFood(string calldata _name, uint256 _price) external {
+    foods.push(Food(_name, _price));
+  }
+
+  function checkAns() external view returns (string memory, uint256) {
+    return (foods[0].name, foods[0].price);
+  }
+
+}
