0047868940456d668f57655d5dd4904be48fe7fa
chen-yanlong <alan900801@gmail.com>
2023-02-28 19:18:16 +0800

    add problem18,19,20 and modified descriptions

---
+++ problemVersion1/18/answer18.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache License
+pragma solidity ^0.8.17;
+
+interface IERC165 {
+    function supportsInterface(bytes4 interfaceID) external view returns (bool);
+}
+
+interface IAnother {
+  function aFunction() external returns (bool);
+  function bFunction() external returns (bool);
+}
+
+// IERC165: 0x01ffc9a7
+// IAnother: 0x579ace26
+contract answer18 is IERC165, IAnother {
+
+    function supportsInterface(bytes4 interfaceId) external pure returns (bool) {
+        return
+            interfaceId == type(IERC165).interfaceId ||
+            interfaceId == this.aFunction.selector ^ this.bFunction.selector;
+    }
+
+    function aFunction() external returns (bool) {}
+    function bFunction() external returns (bool) {}
+
+    function getIAnotherId() external pure returns(bytes4) {
+      return this.aFunction.selector ^ this.bFunction.selector;
+    }
+}
+
+
