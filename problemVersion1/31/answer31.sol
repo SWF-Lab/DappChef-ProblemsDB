c2d88626ab59de26c0d8ea73c074bcac10e5f224
FoodChain1028 <sh1001309@gmail.com>
2023-03-02 01:45:59 +0800

    revise

--- problemVersion1/31/answer31.sol
+++ problemVersion1/31/answer31.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.17;
 
 contract answer31 {
 
-    // TODO: given an uint8, a, you should add 234 to `a` but should also make sure the compiler will not occur error.
+    // TODO: given an uint8, a, you should return a value with `a+234`, but should also make sure the compiler will not occur error.
     function overflow(uint8 a) public pure returns(uint8) {
         unchecked{           
             return a + 234;
