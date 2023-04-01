63cffd2d2ab2e148050193e2727c82fd782f04fd
FoodChain1028 <sh1001309@gmail.com>
2023-04-01 20:41:19 +0800

    rewrite 35

--- problemVersion1/35/answer35.sol
+++ problemVersion1/35/answer35.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.17;
 
 contract answer35 {
     function stringConcat(string memory a, string memory b, string memory c) public pure returns(string memory) {
-        return string.concat(a, " ", b, " ", c);
+        return string.concat(a, " ", b, " ", c, ".");
     }
 }
